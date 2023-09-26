004d7a7fc150907b90d061a5d287c80fb0be6ebd
imKota <aleksey-nyaka@yandex.ru>
2023-09-26 21:28:30 +0300

    add

---
+++ CriwareXtxDecoder/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Markup;
+using System.Drawing.Imaging;
+
+namespace CriwareXtxDecoder
+{
+    class Program
+    {
+        static System.Drawing.Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new PngBitmapEncoder();
+
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+            }
+            return bitmap;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.ToString());
+                return;
+            }
+            var TxtObject = new Cri.XtxFormat();
+            var metaData = TxtObject.ReadMetaData(fs);
+            if (metaData != null)
+            {
+                ImageData BitmapObject = null;
+
+                try
+                {
+                    BitmapObject = TxtObject.Read(fs, metaData);
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.ToString());
+                    return;
+                }
+
+                string outFileName = args[0] + ".png";
+
+                var bitmap = BitmapFromSource(BitmapObject.Bitmap);
+                bitmap.Save(outFileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+    }
+}
